76d6111330a8d778f0c550068b223a5c80e30eb3
Dhruv <dhruvchitkara27@gmail.com>
2019-01-10 02:57:50 +0530

    Generate user data

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
 
 router.post('/', (req,res,next) => {
+    User.create(req.body, (err, user) => {
+      if(err){
+        res.json({
+          confirmation: 'fail',
+          error: err
+        })
+        return
+      }
+
       res.json({
-        data: req.body
+        confirmation: 'success',
+        user: user
       })
+    })
 })
 
 module.exports = router
