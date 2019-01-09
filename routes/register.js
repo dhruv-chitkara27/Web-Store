450ae3d1a52d26f1d241a6b2c268bf149b107a79
Dhruv <dhruvchitkara27@gmail.com>
2019-01-10 02:45:50 +0530

    Coonecting mongoose

---
+++ routes/register.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/', (req,res,next) => {
+      res.json({
+        data: req.body
+      })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const home = require('./routes/home')
+const register = require('./routes/register')
+
+mongoose.connect('mongodb://localhost/Web-Store', (err, data) => {
+  if(err){
+    console.log('DB Connection Failed')
+    return
+  }
+  console.log('DB Connection Success')
+})
 
 const app = express()
 app.set('views' , path.join(__dirname, 'views'))
 app.set('view engine' , 'hjs')
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/',home)
+app.use('/register', register)
 
 app.listen(5000)
 console.log('App running on http://localhost:5000')
