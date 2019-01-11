33b9d44a6ecc887d755525b0d5bc13d065f32c41
Dhruv <dhruvchitkara27@gmail.com>
2019-01-12 03:07:32 +0530

    user

--- config/auth.js
+++ config/auth.js
@@ -18,5 +18,29 @@ module.exports = (passport) => {
     passwordField: 'password',
     passReqToCallback; true
   }, (req, email, password, next) => {
+
+    User.findOne({email: email}, (err, user) => {
+      if (err){
+        return next(err)
+      }
+
+      // user not found:
+      if (user == null)
+        return next(new Error('User Not Found'))
+
+      // check password:
+      if (user.password != req.body.password){
+        return next(new Error('Incorrect Password'))
+      }
+        return next(null, user)
+      //res.json({
+        //confirmation:'success',
+        //user: user
+      //})
+    })
+
+
+  })
+
   })
 }
--- routes/login.js
+++ routes/login.js
@@ -2,30 +2,5 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 
-router.post('/', (req, res, next) => {
-	const email = req.body.email
-
-	User.findOne({email: email}, (err, user) => {
-		if (err){
-			return next(err)
-		}
-
-		// user not found:
-		if (user == null)
-			return next(new Error('User Not Found'))
-
-		// check password:
-		if (user.password != req.body.password){
-			return next(new Error('Incorrect Password'))
-		}
-
-		res.json({
-			confirmation:'success',
-			user: user
-		})
-	})
-
-
-})
 
 module.exports = router
