821418eeb3209594fb8927f4535577cd6ebe7aaa
Dhruv <dhruvchitkara27@gmail.com>
2019-01-13 03:46:09 +0530

    account.hjs

--- routes/account.js
+++ routes/account.js
@@ -2,16 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res,next) => {
-    res.json({
-      user: req.user || 'not logged in'
-    })
+  const user = req.user
+  if(user == null){
+    res.redirect('/')
+    return
+  }
+  const data = {
+    user: user
+  }
+  res.render('account',data)
   })
 
 router.get('/logout', (req,res,next) => {
   req.logout()
-  res.json({
-    confirmation: 'user logged out'
-  })
+  res.redirect('/')
 })
 
 module.exports = router
