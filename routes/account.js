a73daa43a0a43dd9d2f5d736bf0903d6ae1ec8fe
Dhruv <dhruvchitkara27@gmail.com>
2019-01-13 17:01:27 +0530

    Items view

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const items = [
+  {name:'Item 1', description:'', price:10},
+  {name:'Item 2', description:'', price:20},
+  {name:'Item 3', description:'', price:15},
+  {name:'Item 4', description:'', price:50},
+  {name:'Item 5', description:'', price:35},
+  {name:'Item 6', description:'', price:100}
+]
 
 router.get('/', (req,res,next) => {
   const user = req.user
@@ -8,7 +16,8 @@ router.get('/', (req,res,next) => {
     return
   }
   const data = {
-    user: user
+    user: user,
+    items: items
   }
   res.render('account',data)
   })
