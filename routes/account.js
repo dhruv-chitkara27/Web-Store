27cd36f1bc00690be816cd998185eb28043c4fdf
Dhruv <dhruvchitkara27@gmail.com>
2019-01-13 23:23:12 +0530

    showing saved items

--- routes/account.js
+++ routes/account.js
@@ -1,60 +1,58 @@
 const express = require('express')
 const router = express.Router()
 const Item = require('../models/Item')
-/*
-const items = [
-  {name:'Item 1', description:'', price:10},
-  {name:'Item 2', description:'', price:20},
-  {name:'Item 3', description:'', price:15},
-  {name:'Item 4', description:'', price:50},
-  {name:'Item 5', description:'', price:35},
-  {name:'Item 6', description:'', price:100}
-]
-*/
-router.get('/', (req,res,next) => {
-  const user = req.user
-  if(user == null){
-    res.redirect('/')
-    return
-  }
-  Item.find(null, (err, items) => {
-    if(err)
-      return next(err)
 
-    const data = {
-      user: user,
-      items: items
-    }
-    res.render('account', data)
-  })
-  })
+router.get('/', (req, res, next) => {
+	const user = req.user
+	if (user == null){
+		res.redirect('/')
+		return
+	}
+
+	Item.find(null, (err, items) => {
+		if (err)
+			return next(err)
+
+		Item.find({interested: user._id}, (err, interestedItems) => {
+			if (err)
+				return next(err)
+
+			const data = {
+				user: user,
+				items: items,
+				interested: interestedItems
+			}
+
+			res.render('account', data)
+		})
+	})
+})
 
 router.get('/additem/:itemid', (req, res, next) => {
-  const user = req.user
-  if(user == null){
-    res.redirect('/')
-    return
-  }
+	const user = req.user
+	if (user == null){
+		res.redirect('/')
+		return
+	}
 
-  Item.findById(req.params.itemid, (err, item) => {
-    if(err){
-      return next(err)
-    }
+	Item.findById(req.params.itemid, (err, item) => {
+		if (err){
+			return next(err)
+		}
 
-    if(item.interested.indexOf(user._id) == -1){
-      item.interested.push(user._id)
-      item.save()
-      res.json({
-        item: item
-      })
-    }
-    })
-  })
+		if (item.interested.indexOf(user._id) == -1){
+			item.interested.push(user._id)
+			item.save()
+			res.redirect('/account')
+		}
 
+	})
 
-router.get('/logout', (req,res,next) => {
-  req.logout()
-  res.redirect('/')
+})
+
+router.get('/logout', (req, res, next) => {
+	req.logout()
+	res.redirect('/')
 })
 
 module.exports = router
