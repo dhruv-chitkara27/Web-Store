ee5dfc89681f993235e45c2fd58c035421a4b0a3
Dhruv <dhruvchitkara27@gmail.com>
2019-01-14 00:46:54 +0530

    Sending Emails

--- routes/account.js
+++ routes/account.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const Mailgun = require('mailgun-js')
 const Item = require('../models/Item')
 const User = require('../models/User')
 
 const randomString = (length) => {
-  let text = ''
-  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-  for (let i=0; i<length; i++){
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
-  }
-  return text
+	let text = ''
+	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	for (let i=0; i<length; i++){
+		text += possible.charAt(Math.floor(Math.random() * possible.length))
+	}
+
+	return text
 }
 
 router.get('/', (req, res, next) => {
@@ -55,9 +57,7 @@ router.get('/additem/:itemid', (req, res, next) => {
 			item.save()
 			res.redirect('/account')
 		}
-
 	})
-
 })
 
 router.get('/logout', (req, res, next) => {
@@ -67,19 +67,42 @@ router.get('/logout', (req, res, next) => {
 
 router.post('/resetpassword', (req, res, next) => {
 
-  User.findOne({email: req.body.email}, (err, user) =>{
-    if(err)
-      return next(err)
+	User.findOne({email: req.body.email}, (err, user) => {
+		if (err)
+			return next(err)
 
-    user.nonce = randomString(8)
-    user.passwordResetTime = new Date()
-    user.save()
-    res.json({
-      confirmation: 'success',
-      data: 'reset password endpoint',
-      email: user
-    })
-  })
+		user.nonce = randomString(8)
+		user.passwordResetTime = new Date()
+		user.save()
+
+		const mailgun = Mailgun({
+			apiKey: 'd199003bbe4a37d0ae84bd27cdfb871a-060550c6-773726af',
+			domain: 'sandbox321b2d4e39dd4305b9aeddd472a101cf.mailgun.org'
+		})
+
+		const data = {
+			to: req.body.email,
+			from: 'dhruvchitkara27@gmail.com',
+			sender: 'Web-Store',
+			subject: 'Password Reset Request',
+			html: 'Please click <a style="color:red" href="http://localhost:5000/account/password-reset?nonce='+user.nonce+'&id='+user._id+'">HERE</a> to reset your password. This link is valid for 24 hours.'
+		}
+
+		mailgun.messages().send(data, (err, body) => {
+			if (err)
+				return next(err)
+
+			// success:
+			res.json({
+				confirmation: 'success',
+				data: 'reset password endpoint',
+				user: user
+			})
+		})
+
+
+	})
+
 })
 
 module.exports = router
